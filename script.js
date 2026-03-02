9bac0f7c9ddcc838b80dad2d7bf47809ab02bf8a
LakshithaDev <lakshitha.dev.contact@gmail.com>
2026-03-02 13:54:21 +0530

    Initial commit -Version 1.0 code and screenshots uploaded

---
+++ script.js
@@ -0,0 +1,8 @@
+// Listen for a click on the 'searchBtn' button
+document.getElementById('searchBtn').addEventListener('click', function() {
+    
+    // Display an alert message when the button is clicked
+    alert("Subtitle search feature coming soon in Version 1.1! 🎬"); 
+
+    // Future logic for finding subtitles can be added here
+});
